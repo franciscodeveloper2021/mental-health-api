59f8ae4e14a01cf6fdc516bce14d08411ee8526f
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-20 03:36:57 -0300

    add: conttroller and i18n text

---
+++ app/controllers/evaluateds_controller.rb
@@ -0,0 +1,27 @@
+class EvaluatedsController < ApplicationController
+
+  def initialize
+    super
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def evaluated_params
+    params.require(:evaluated).permit(:name, :cpf, :email)
+  end
+end
