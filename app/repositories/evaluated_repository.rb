ea1470393e600e3f15088a5cf8643b24f8a4a582
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-20 06:37:40 -0300

    add: repo done

--- app/repositories/evaluated_repository.rb
+++ app/repositories/evaluated_repository.rb
@@ -1,24 +1,36 @@
 class EvaluatedRepository
-  def index
+
+  def find_all_evaluateds
     evaluateds = Evaluated.all
     return I18n.t("warnings.nothing", entity: "evaluateds") if evaluateds.empty?
 
     evaluateds
   end
 
-  def show(evaluated_id)
+  def find_evaluated(evaluated_id)
     evaluated = Evaluated.find_by_id(evaluated_id)
     raise ActiveRecord::RecordNotFound, I18n.t("errors.not_found", record: "Evaluated", attribute: evaluated_id) if evaluated.nil?
 
     evaluated
   end
 
-  def create
+  def create(params)
+    evaluated = Evaluated.new(params)
+
+    evaluated.save!
+
+    evaluated
   end
 
-  def update
+  def update(evaluated_id, params)
+    evaluated = find_evaluated(evaluated_id)
+
+    evaluated.update(params)
   end
 
-  def destroy
+  def destroy(evaluated_id)
+    evaluated = find_evaluated(evaluated_id)
+
+    evaluated.destroy
   end
 end
