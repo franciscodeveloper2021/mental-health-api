f34acae711058034516fdbd18bb37e10394ccd0f
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-23 00:53:21 -0300

    add: removing for a while

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -12,18 +12,4 @@ class Instrument < ApplicationRecord
             length: { minimum:10, maximum: 300 }
 
   enum status: { pendent: 0, done: 1 }
-
-  after_create :update_status
-
-  def update_status
-    new_status = if questions.count.zero? || questions.joins(:answer).distinct.count.zero?
-      :pendent
-    elsif questions.count == questions.joins(:answer).distinct.count
-      :done
-    else
-      :pendent
-    end
-
-    update_columns(status: new_status)
-  end
 end
