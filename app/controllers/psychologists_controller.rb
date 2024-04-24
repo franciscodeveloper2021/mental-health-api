f90983913930ef7c8e7f84670d69000c318910ae
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-24 05:38:39 -0300

    add: wip

--- app/controllers/psychologists_controller.rb
+++ app/controllers/psychologists_controller.rb
@@ -4,12 +4,23 @@ class PsychologistsController < ApplicationController
     super
 
     @service = UseCases::Psychologists::CreatePsychologistService.new
+    @login_authentication_service = Authentication::LoginAuthenticationService.new
   end
 
   def create
     psychologist = @service.create(psychologist_params)
 
     render json: psychologist, status: :created
+  end
+
+  def log_in
+    result = @login_authentication_service.perform(params[:name], params[:password])
+
+    if result[:token]
+      render json: result
+    else
+      render json: result, status: :unauthorized
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :psychologists
+  post 'login', to: 'psychologists#log_in'
   resources :evaluateds do
     member do
       post 'assign_instrument'
